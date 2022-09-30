0687cd8368fe4d8d7ac1def69a04e6ec3deee768
baokhoa-code <khoa123456789bork@gmail.com>
2022-09-30 14:00:23 +0700

    Initially upload project

---
+++ JobsManagementApp/forgot.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JobsManagementApp
+{
+    /// <summary>
+    /// Interaction logic for forgot.xaml
+    /// </summary>
+    public partial class forgot : Window
+    {
+        public forgot()
+        {
+            InitializeComponent();
+        }
+
+        private void cbx_question_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
