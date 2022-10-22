27219f3833b2f23f4fba99f3e0ee740fbbdefdf2
baokhoa-code <khoa123456789bork@gmail.com>
2022-10-22 19:59:13 +0700

    Update

---
+++ JobsManagementApp/View/Admin/DashBoard/JobAddWindowAdmin.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JobsManagementApp.View.Admin.DashBoard
+{
+    /// <summary>
+    /// Interaction logic for JobAddWindowAdmin.xaml
+    /// </summary>
+    public partial class JobAddWindowAdmin : Window
+    {
+        public JobAddWindowAdmin()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ JobsManagementApp/View/Admin/DashBoard/ReportAddWindowAdmin.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JobsManagementApp.View.Admin.DashBoard
+{
+    /// <summary>
+    /// Interaction logic for ReportAddWindowAdmin.xaml
+    /// </summary>
+    public partial class ReportAddWindowAdmin : Window
+    {
+        public ReportAddWindowAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
